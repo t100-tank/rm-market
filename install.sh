441788e7f8bbc0df3487a8f9de29d317d55e7a9a
t100-tank <t100.tank@gmail.com>
2013-11-30 17:51:23 +0200

    install script

--- install.sh
+++ install.sh
@@ -6,6 +6,8 @@ unzip lib/vendor/symfony.zip -d lib/vendor
 ./symfony project:permissions
 ./symfony cc
 
-echo "Sozdat' bazu, i nazvarie+user+pass vpisat v config/databases.yml"
+echo ""
+echo "Sozdat' bazu, i nazvanie+user+pass vpisat v config/databases.yml"
 echo "Potom zapustit':"
 echo "./symfony propel:build-all-load --no-confirmation"
+echo ""
