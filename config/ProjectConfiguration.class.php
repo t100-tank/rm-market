696e21fdded038ab2c502a1310d25184b09c2691
t100-tank <t100.tank@gmail.com>
2013-11-30 17:32:07 +0200

    config added

---
+++ config/ProjectConfiguration.class.php
@@ -0,0 +1,17 @@
+<?php
+ini_set('max_execution_time', 180);
+require_once dirname(__FILE__) . '/../lib/vendor/symfony/lib/autoload/sfCoreAutoload.class.php';
+sfCoreAutoload::register();
+
+class ProjectConfiguration extends sfProjectConfiguration {
+
+    public function setup() {
+        $this->enablePlugins('sfPropelPlugin');
+        $this->enablePlugins('sfGuardPlugin');
+        $this->enablePlugins('sfFormExtraPlugin');
+        $this->enablePlugins('sfImageTransformPlugin');
+//        if (isset($_SERVER['DOCUMENT_ROOT']))
+//            $this->setWebDir($_SERVER['DOCUMENT_ROOT']);
+    }
+
+}
