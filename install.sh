51a7c6fcbb6c4dbaea428a01fd1080c65837d754
t100-tank <t100.tank@gmail.com>
2013-11-30 17:48:21 +0200

    install script

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 mkdir log cache test web/uploads
 cp config/databases.example.yml config/databases.yml
+unzip lib/vendor/symfony.zip -d lib/vendor
 
-echo "Run the following:"
-echo "./symfony cc"
-echo "./symfony project:permissions"
+./symfony project:permissions
+./symfony cc
+
+echo "Sozdat' bazu, i nazvarie+user+pass vpisat v config/databases.yml"
+echo "Potom zapustit':"
+echo "./symfony propel:build-all-load --no-confirmation"
