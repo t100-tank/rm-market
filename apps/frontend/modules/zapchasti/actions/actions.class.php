b4d7d9af50b4116966ac319413b865176051fce4
t100-tank <t100.tank@gmail.com>
2013-12-03 22:16:27 +0200

    label action: all info is retrieved

--- apps/frontend/modules/zapchasti/actions/actions.class.php
+++ apps/frontend/modules/zapchasti/actions/actions.class.php
@@ -15,9 +15,54 @@ class zapchastiActions extends sfActions {
     }
 
     public function executeLabel(sfWebRequest $request) {
-        $label = CarLabelPeer::retrieveBySlug($request->getParameter('car_label'));
-        if (!$label)
+        $this->label = CarLabelPeer::retrieveBySlug($request->getParameter('car_label'));
+        if (!$this->label)
             $this->redirect404();
+
+        $opt = $this->getContext()->getRouting()->getOptions('context');
+        $link = $opt['context']['path_info'];
+        $this->page = PagesPeer::retrieveBySlug($link);
+
+        $this->breadcrumb = array(
+            array(
+                'link' => '@homepage',
+                'title' => 'Главная'
+            )
+        );
+        if ($this->page instanceof Pages) {
+            $this->getResponse()->setTitle($this->page->getTitle());
+            $this->getResponse()->addMeta('description', $this->page->getMetaDescription());
+            $this->getResponse()->addMeta('keywords', $this->page->getMetaKeywords());
+
+            $breadcrumb = json_decode($this->page->getBreadcrumb(), true);
+            if (is_array($breadcrumb)) {
+                foreach ($breadcrumb as $item) {
+                    $this->breadcrumb[] = array(
+                        'link' => (!empty($item['link'])) ? $item['link']: null,
+                        'title' => (!empty($item['title'])) ? $item['title']: ''
+                    );
+                }
+            } else {
+                $this->breadcrumb[] = array(
+                    'link' => null,
+                    'title' => $this->page->getBreadcrumb()
+                );
+            }
+        } else {
+            $this->getResponse()->setTitle($this->label->getName());
+            $this->getResponse()->addMeta('description', $this->label->getName());
+            $this->getResponse()->addMeta('keywords', $this->label->getName());
+
+            $this->breadcrumb[] = array(
+                    'link' => '@zapchasti',
+                    'title' => 'Автозапчасти'
+                );
+            $this->breadcrumb[] = array(
+                    'link' => null,
+                    'title' => $this->label->getName()
+                );
+        }
+
     }
 
 }
