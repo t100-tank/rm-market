eba05273b21baac6f8d0dc32710df35da7e194e5
t100-tank <t100.tank@gmail.com>
2013-12-07 13:14:13 +0200

    category tree: sub-category selected

--- apps/frontend/modules/zapchasti/templates/_tree.php
+++ apps/frontend/modules/zapchasti/templates/_tree.php
@@ -2,15 +2,23 @@
 <?php if (count($tree)) { ?>
     <ul class="top-list">
         <?php foreach ($tree as $item) { ?>
-            <li class="top-item<?php echo $item['id'] == $categoryId ? ' toggled': ''; ?>">
-                <a class="top-link<?php echo $item['id'] == $categoryId ? ' pre-selected selected': ''; ?>" href="<?php echo url_for('zapchasti_label_category', array('car_label' => $carLabel, 'category' => $item['slug'])); ?>"><?php echo $item['name'] ?></a>&nbsp;<span class="toggle"><?php echo $item['id'] == $categoryId ? '&raquo;': '&laquo;'; ?></span>
-                <?php if (count($item['sub_items'])) { ?>
-                    <br/><ul class="sub-list">
-                        <?php foreach ($item['sub_items'] as $subItem) { ?>
-                            <li class="sub-item">&rsaquo; <a class="sub-link<?php echo $subItem['id'] == $categoryId ? ' selected': ''; ?>" href="<?php echo url_for('zapchasti_label_category', array('car_label' => $carLabel, 'category' => $subItem['slug'])); ?>"><?php echo $subItem['name'] ?></a></li>
-                        <?php } ?>
-                    </ul>
-                <?php } ?>
+            <?php
+            $subList == '';
+            $matched = false;
+            if (count($item['sub_items'])) {
+                $subList .= '<br/><ul class="sub-list">';
+                foreach ($item['sub_items'] as $subItem) {
+                    $matched |= $subItem['id'] == $categoryId;
+                    $subList .= '<li class="sub-item">&rsaquo; <a class="sub-link'.($subItem['id'] == $categoryId ? ' selected': '').'" href="'.url_for('zapchasti_label_category', array('car_label' => $carLabel, 'category' => $subItem['slug'])).'">'.$subItem['name'].'</a></li>';
+                }
+                $subList .= '</ul>';
+            }
+            $matched |= $item['id'] == $categoryId;
+            ?>
+            <li class="top-item<?php echo $matched ? ' toggled': ''; ?>">
+                <a class="top-link<?php echo $matched ? ' pre-selected selected': ''; ?>" href="<?php echo url_for('zapchasti_label_category', array('car_label' => $carLabel, 'category' => $item['slug'])); ?>"><?php echo $item['name'] ?></a>&nbsp;<span class="toggle"><?php echo $matched ? '&raquo;': '&laquo;'; ?></span>
+                <?php echo $subList; ?>
+                <?php unset($subList); ?>
             </li>
         <?php } ?>
     </ul>
