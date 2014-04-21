d1947f932050a1e0cde0d9a38ef782a097c0f329
t100-tank <t100.tank@gmail.com>
2014-04-21 11:20:07 +0300

    remote changes + phone -> ya-phone

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -1,4 +1,4 @@
-<?php $route = $sf_context->getInstance()->getRouting()->getCurrentRouteName(); ?>
+﻿<?php $route = $sf_context->getInstance()->getRouting()->getCurrentRouteName(); ?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -22,8 +22,8 @@
                             </div>
                             <div class="col-md-8 col-sm-8 col-xs-12 header-right">
                                 <a href="#" id="favourites" rel="sidebar">Добавить в избранное</a><br/>
-                                <span class="address"><strong>107497, Москва, Амурская ул.,Дом 5, стр.1</strong></span><br/>
-                                <span class="tele"><strong>+7(495)737-89-04</strong></span><br/>
+                                <span class="address"><strong>107497, Москва, Амурская ул., дом 5, стр.1</strong></span><br/>
+                                <span class="tele ya-phone"><strong>+7(495)737-89-04</strong></span><br/>
                                 <span class="schedule"><strong>c 9.00 до 21.00 без выходных</strong></span>
                             </div>
                             <div class="col-md-8 col-sm-12 col-xs-12 header-menu">
@@ -44,7 +44,7 @@
                                                 <ul class="dropdown-menu">
                                                     <li><a href="<?php echo url_for_ext('@content_page?slug=history/'); ?>">История</a></li>
                                                     <li><a href="/progress/">Карьера и вакансии</a></li>
-                                                    <li><a href="/progress/">Галерея</a></li>
+                                                    <li><a href="/gallery/">Галерея</a></li>
                                                 </ul>
                                             </li>
                                             <li class="div"><a href="/progress/" class="top-nav news">Новости</a></li>
@@ -71,7 +71,7 @@
                                 <a data-toggle="modal" href="<?php echo url_for('@form_show?slug=appointment'); ?>" class="tablet">Запись на ремонт on-line</a>
                             </div>
                             <div class="sec-menu-item">
-                                <a href="/progress/" class="gears">Поиск автозапчастей</a>
+                                <a href="/avtozapchasti/" class="gears">Поиск автозапчастей</a>
                             </div>
                             <div class="sec-menu-item">
                                 <a href="<?php echo url_for_ext('@content_page?slug=corporate/'); ?>" class="network">Корпоративным клиентам</a>
@@ -302,7 +302,7 @@
                                             </div>
                                             <div class="col-md-4 col-sm-6 col-xs-12 u-item">
                                                 <div class="corner lt"></div><div class="corner lb"></div><div class="corner rt"></div><div class="corner rb"></div>
-                                                <a href="/progress/" class="ustanovka-dop" title="Установка доп. оборудования">
+                                                <a href="/ustanovka_dop_oborudovaniya/" class="ustanovka-dop" title="Установка доп. оборудования">
                                                     <span class="ico"></span>
                                                     <span class="name">Установка доп. оборудования</span>
                                                 </a>
@@ -361,7 +361,7 @@
                             <a data-toggle="modal" href="<?php echo url_for('@form_show?slug=appointment'); ?>" class="tablet">Запись на ремонт on-line</a>
                         </div>
                         <div class="col-md-3 col-sm-6 col-xs-12 sec-menu-item">
-                            <a href="/progress/" class="gears">Поиск автозапчастей</a>
+                            <a href="/avtozapchasti/" class="gears">Поиск автозапчастей</a>
                         </div>
                         <div class="col-md-3 col-sm-6 col-xs-12 sec-menu-item">
                             <a href="<?php echo url_for_ext('@content_page?slug=corporate/'); ?>" class="network">Корпоративным клиентам</a>
@@ -416,7 +416,7 @@
                                         <ul class="dropdown-menu">
                                             <li><a tabindex="-1" href="<?php echo url_for_ext('@content_page?slug=history/'); ?>">История</a></li>
                                             <li><a tabindex="-1" href="#">Карьера и вакансии</a></li>
-                                            <li><a tabindex="-1" href="#">Галерея</a></li>
+                                            <li><a tabindex="-1" href="<?php echo url_for_ext('@content_page?slug=gallery/'); ?>">Галерея</a></li>
                                         </ul>
                                     </li>
                                     <li><a href="/progress/">Новости</a></li>
@@ -426,7 +426,7 @@
                                     <li><a data-toggle="modal" href="<?php echo url_for('@form_show?slug=backCall'); ?>">Заказать обратный звонок</a></li>
                                     <li><a data-toggle="modal" href="<?php echo url_for('@form_show?slug=appointment'); ?>">Запись на ремонт on-line</a></li>
                                     <li><a href="<?php echo url_for_ext('@content_page?slug=technichesky-likbez/'); ?>">Технический ликбез</a></li>
-                                    <li><a href="/progress/">Поиск автозапчастей</a></li>
+                                    <li><a href="<?php echo url_for_ext('@content_page?slug=avtozapchasti/'); ?>">Поиск автозапчастей</a></li>
                                     <li><a href="<?php echo url_for_ext('@content_page?slug=corporate/'); ?>">Корпоративным клиентам</a></li>
                                     <li><a href="<?php echo url_for_ext('@content_page?slug=auxpage_cafe/'); ?>">Кафе</a></li>
                                 </ul>
@@ -461,7 +461,7 @@
                                     <li class="smaller">&gt; <a href="http://gbo-ustanovka.ru">Установка ГБО</a></li>
                                     <li class="smaller">&gt; <a href="<?php echo url_for_ext('@content_page?slug=shinomontag/'); ?>">Шиномонтаж</a></li>
                                     <li class="smaller">&gt; <a href="/progress/">Кредитование</a></li>
-                                    <li class="smaller">&gt; <a href="/progress/">Установка доп. оборудования</a></li>
+                                    <li class="smaller">&gt; <a href="<?php echo url_for_ext('@content_page?slug=ustanovka_dop_oborudovaniya/'); ?>">Установка доп. оборудования</a></li>
                                     <li class="smaller">&gt; <a href="<?php echo url_for_ext('@content_page?slug=evakuator/'); ?>">Эвакуация</a></li>
                                     <li class="smaller">&gt; <a href="<?php echo url_for_ext('@content_page?slug=podbor/'); ?>">Помощь в подборе авто</a></li>
                                     <li class="smaller">&gt; <a href="/progress/">Сигнализация и охранные системы</a></li>
@@ -472,8 +472,8 @@
                             <div class="col-md-3 col-sm-6">
                                 <ul>
                                     <li>107497, Москва</li>
-                                    <li>Амурская ул., Дом 5, стр.1</li>
-                                    <li class="tele">+7(495)737-89-04</li>
+                                    <li>Амурская ул., дом 5, стр.1</li>
+                                    <li class="tele ya-phone">+7(495)737-89-04</li>
                                     <li class="social">
                                         <a class="fb" href="http://facebook.com/"></a><a class="vk" href="http://vk.com/"></a><a class="gp" href="http://plus.google.com/"></a>
                                     </li>
@@ -507,6 +507,33 @@
                 })();
 
             </script>
+            <!-- Yandex.Metrika counter -->
+            <script type="text/javascript">
+            (function (d, w, c) {
+                (w[c] = w[c] || []).push(function() {
+                    try {
+                        w.yaCounter24591170 = new Ya.Metrika({id:24591170,
+                                webvisor:true,
+                                clickmap:true,
+                                trackLinks:true,
+                                accurateTrackBounce:true});
+                    } catch(e) { }
+                });
+
+                var n = d.getElementsByTagName("script")[0],
+                    s = d.createElement("script"),
+                    f = function () { n.parentNode.insertBefore(s, n); };
+                s.type = "text/javascript";
+                s.async = true;
+                s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
+
+                if (w.opera == "[object Opera]") {
+                    d.addEventListener("DOMContentLoaded", f, false);
+                } else { f(); }
+            })(document, window, "yandex_metrika_callbacks");
+            </script>
+            <noscript><div><img src="//mc.yandex.ru/watch/24591170" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+            <!-- /Yandex.Metrika counter -->
         <?php } ?>
     </body>
 </html>
